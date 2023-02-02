4a5ab2d1705df6c7f3ef59746fb54e132d842d67
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-02-02 15:48:30 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,27 @@
+export const item = {
+    "id": "e6797e21-1f65-47c2-9741-21ddfdcaa174",
+    "gallery": [
+        {
+            "id": "0bd34dc3-960a-44e4-bbb4-031515e56a46",
+            "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/304.png"
+        },
+        {
+            "id": "53c4e554-8c57-43bb-92f0-66384792239c",
+            "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/256.png"
+        },
+        {
+            "id": "1d5baad3-1c5a-47ed-b656-acfe3b1c1edb",
+            "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/79.png"
+        },
+        {
+            "id": "ecaf7ce4-b062-4921-a011-6a85ec104e6b",
+            "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/237.png"
+        },
+        {
+            "id": "29bf809e-75c6-469f-95a8-79b48e19066d",
+            "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/237.png"
+        }
+    ],
+    "event_name": "carry",
+    "event_description": "Effort work himself operation little. Only wife everyone phone production move. Necessary second notice board."
+}
